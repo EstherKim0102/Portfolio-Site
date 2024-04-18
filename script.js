1abc293db24ef42ac0a21cc57d55c10e9c7ba20b
Esther Kim <estherkim@uconn.edu>
2024-04-17 23:56:26 -0400

    website update - about me

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ let navLinks = document.querySelectorAll('.nav-lnks li');
 
 
 document.addEventListener('mousemove', (event) => {
-    const mouseX = event.clientX; // Get the mouse X-coordinate
-    const mouseY = event.clientY; // Get the mouse Y-coordinate
+    const mouseX = event.pageX; // Get the mouse X-coordinate
+    const mouseY = event.pageY; // Get the mouse Y-coordinate
 });
 
 
@@ -21,8 +21,8 @@ document.addEventListener('click', function(event) {
         const size = Math.random() * 40 + 10;
         const angle = Math.random() * 360;
         const distance = Math.random() * 50 + 10;
-        const x = event.clientX + Math.cos(angle) * distance;
-        const y = event.clientY + Math.sin(angle) * distance;
+        const x = event.pageX + Math.cos(angle) * distance;
+        const y = event.pageY + Math.sin(angle) * distance;
 
         clover.style.left = x + 'px';
         clover.style.top = y + 'px';
@@ -37,96 +37,96 @@ document.addEventListener('click', function(event) {
 
 
       
-   const cursor = document.querySelector(".cursor");
+//    const cursor = document.querySelector(".cursor");
 
-    //follow cursor on mouse move
-    document.addEventListener("mousemove", (e) => {
-        let x = e.pageX;
-        let y = e.pageY;
+//     //follow cursor on mouse move
+//     document.addEventListener("mousemove", (e) => {
+//         let x = e.pageX;
+//         let y = e.pageY;
 
-        cursor.style.top = y + "px";
-        cursor.style.left = x + "px";
-        cursor.style.display = "block";
+//         cursor.style.top = y + "px";
+//         cursor.style.left = x + "px";
+//         cursor.style.display = "block";
 
-        //cursor effects on mouse stopped
-        function mouseStopped(){
-            cursor.style.display = "none";
-        }
-        timeout = setTimeout(mouseStopped, 1000);
-    });
-
-    //cursor effects on mouseout
-    document.addEventListener("mouseout", () => {
-        cursor.style.display = "none";
-    });
-
-
-// circles.forEach(function(circle, index){
-//     circle.x = 0;
-//     circle.y = 0;
-//     circle.style.backgroundColor = colors[index % colors.length];
-// });
-
-// window.addEventListener("mousemove", function(ev){
-//     coords.x = ev.clientX;
-//     coords.y = ev.clientY;
-    // console.log(ev.clientX, ev.clientY);
-      
-    // circle.style.top = y + "px";
-    // circle.style.left = x + "px";
-    // circle.style.display = "block";
-
-    // //cursor effects on mouse stopped
-    // function mouseStopped(){
-    //     circle.style.display = "none";
-    // }
-    // // cleaerTimeout(timeout);
-    // timeout = setTimeout(mouseStopped, 1000);
-    // //cursor effects on mouseout
-    // document.addEventListener("mouseout", () => {
-    //     circle.style.display = "none";
-    // });
-
-// });
-
-
-// animateCircles();
-
-
-// function animateCircles() {
-//     let x = coords.x;
-//     let y = coords.y;
-
-//     circles.forEach(function (circle, index) {
-//         circle.style.left = x + "px";
-//         circle.style.top = y + "px";
-
-//         circle.style.scale = (circles.length - index) / circles.length;
-
-//         circle.x = x;
-//         circle.y = y;
-
-//         const nextCircle = circles[index + 1] || circles[0];
-//         x += (nextCircle.x - x) * 0.9;
-//         y += (nextCircle.y - y) * 0.9;
-       
+//         //cursor effects on mouse stopped
+//         function mouseStopped(){
+//             cursor.style.display = "none";
+//         }
+//         timeout = setTimeout(mouseStopped, 1000);
 //     });
 
-    // navLinks.forEach(link => {
-    //     link.addEventListener("mouseleave", () =>{
-    //         circles.classList.remove('link-grow');
-    //         circle.classList.remove('link-grow');
-    //     });
-    //     link.addEventListener("mouseover", () =>{
-    //         circles.classList.add('link-grow');
-    //         circle.classList.add('link-grow');
-    //     });
-    // });
+//     //cursor effects on mouseout
+//     document.addEventListener("mouseout", () => {
+//         cursor.style.display = "none";
+//     });
+
+
+circles.forEach(function(circle, index){
+    circle.x = 0;
+    circle.y = 0;
+    circle.style.backgroundColor = colors[index % colors.length];
+});
+
+window.addEventListener("mousemove", function(ev){
+    coords.x = ev.pageX;
+    coords.y = ev.pageY;
+    console.log(ev.clientX, ev.clientY);
+      
+    circle.style.top = y + "px";
+    circle.style.left = x + "px";
+    circle.style.display = "block";
+
+    //cursor effects on mouse stopped
+    function mouseStopped(){
+        circle.style.display = "none";
+    }
+    // cleaerTimeout(timeout);
+    timeout = setTimeout(mouseStopped, 1000);
+    //cursor effects on mouseout
+    document.addEventListener("mouseout", () => {
+        circle.style.display = "none";
+    });
+
+});
+
+
+animateCircles();
+
+
+function animateCircles() {
+    let x = coords.x;
+    let y = coords.y;
+
+    circles.forEach(function (circle, index) {
+        circle.style.left = x + "px";
+        circle.style.top = y + "px";
+
+        circle.style.scale = (circles.length - index) / circles.length;
+
+        circle.x = x;
+        circle.y = y;
+
+        const nextCircle = circles[index + 1] || circles[0];
+        x += (nextCircle.x - x) * 0.9;
+        y += (nextCircle.y - y) * 0.9;
+       
+    });
+
+    navLinks.forEach(link => {
+        link.addEventListener("mouseleave", () =>{
+            circles.classList.remove('link-grow');
+            circle.classList.remove('link-grow');
+        });
+        link.addEventListener("mouseover", () =>{
+            circles.classList.add('link-grow');
+            circle.classList.add('link-grow');
+        });
+    });
 
     
 
-//     requestAnimationFrame(animateCircles);
-// }
+    requestAnimationFrame(animateCircles);
+}
 
 
 
